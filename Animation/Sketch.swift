e73c641d9211d48f6bbc7901bf10829737ee6c70
Liam Davidson <ldavidson@lcs.on.ca>
2017-12-18 16:43:03 -0500

    THIS IS SO FLIPPING FUN!!!!!!

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -6,9 +6,19 @@ class Sketch : NSObject {
     //       Therefore, the line immediately below must always be present.
     let canvas : Canvas
     
-    // Position of circle
+    
+    
+    
+    
+    
+    
+    // creating variables space
     var x : Int
     var xreverse : Int
+    var y = 550
+    var expwidth = 25
+    var hueofexlosion = 60
+    var fadealpha = 100
     // This function runs once
     override init() {
         
@@ -24,61 +34,81 @@ class Sketch : NSObject {
     func draw() {
         
         // Clear the background
-        canvas.fillColor = Color.white
-        // canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: canvas.width, height: canvas.height)
+        canvas.fillColor = Color.init(hue: 190, saturation: 30, brightness: 80, alpha: 100)
+        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: canvas.width, height: canvas.height)
         
-        // Change position
-        x += 1
-        xreverse -= 1
-        // Draw an ellipse in the middle of the canvas
-        canvas.fillColor = Color.black
-        canvas.drawEllipse(centreX: x, centreY: 50, width: 50, height: 50)
-        canvas.drawEllipse(centreX: xreverse, centreY: 150, width: 50, height: 50)
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
-        canvas.drawEllipse(centreX: xreverse, centreY: 350, width: 50, height: 50)
-        canvas.drawEllipse(centreX: x, centreY: 450, width: 50, height: 50)
-        
-        
-        canvas.drawEllipse(centreX: 50, centreY: x, width: 50, height: 50)
-        canvas.drawEllipse(centreX: 150, centreY: xreverse, width: 50, height: 50)
-        canvas.drawEllipse(centreX: 250, centreY: x, width: 50, height: 50)
-        canvas.drawEllipse(centreX: 350, centreY: xreverse, width: 50, height: 50)
-        canvas.drawEllipse(centreX: 450, centreY: x, width: 50, height: 50)
-        
-        
-        
-        
+        //ground
         canvas.fillColor = Color.green
-        canvas.drawEllipse(centreX: x, centreY: 50, width: 20, height: 20)
-        canvas.drawEllipse(centreX: xreverse, centreY: 150, width: 20, height: 20)
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 20, height: 20)
-        canvas.drawEllipse(centreX: xreverse, centreY: 350, width: 20, height: 20)
-        canvas.drawEllipse(centreX: x, centreY: 450, width: 20, height: 20)
+        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: canvas.width, height: 200)
+        
+        //nuke
+        if y > 200{
+        canvas.fillColor = Color.init(hue: 120, saturation: 40, brightness: 60, alpha: 100)
+        canvas.drawEllipse(centreX: 250, centreY: y, width: 25, height: 50)
+        canvas.drawRectangle(centreX: 250, centreY: y + 25, width: 40, height: 15)
+            }
+        
+        //move things down
+        y -= 1
+        
+        //the hole
+        if expwidth > 250 {
+            canvas.fillColor = Color(hue: 190, saturation: 30, brightness: 80, alpha: 100)
+            canvas.drawEllipse(centreX: 250, centreY: 200, width: 250, height: 250)
+        }
         
         
-        canvas.drawEllipse(centreX: 50, centreY: x, width: 20, height: 20)
-        canvas.drawEllipse(centreX: 150, centreY: xreverse, width: 20, height: 20)
-        canvas.drawEllipse(centreX: 250, centreY: x, width: 20, height: 20)
-        canvas.drawEllipse(centreX: 350, centreY: xreverse, width: 20, height: 20)
-        canvas.drawEllipse(centreX: 450, centreY: x, width: 20, height: 20)
+        //explosion
+        if y < 200 {
+            canvas.fillColor = Color.init(hue: hueofexlosion, saturation: 50, brightness: 80, alpha: fadealpha)
+            canvas.drawShapesWithBorders = false
+            canvas.drawEllipse(centreX: 250, centreY: 190, width: expwidth, height: expwidth)
+            
+            //second explosion
+            if expwidth > 20 {
+                
+                canvas.fillColor = Color.init(hue: hueofexlosion - 10, saturation: 50, brightness: 80, alpha: fadealpha)
+                canvas.drawShapesWithBorders = false
+                canvas.drawEllipse(centreX: 250, centreY: 190, width: expwidth - 10, height: expwidth - 10)
+                
+            }
+            
+         //third explosion
+            if expwidth > 30 {
+                
+                canvas.fillColor = Color.init(hue: hueofexlosion - 20, saturation: 50, brightness: 80, alpha: fadealpha)
+                canvas.drawShapesWithBorders = false
+                canvas.drawEllipse(centreX: 250, centreY: 190, width: expwidth - 20, height: expwidth - 20)
+                
+            }
+            
+            //fourth explosion
+            if expwidth > 50 {
+                
+                canvas.fillColor = Color.init(hue: hueofexlosion - 30, saturation: 50, brightness: 80, alpha: fadealpha)
+                canvas.drawShapesWithBorders = false
+                canvas.drawEllipse(centreX: 250, centreY: 190, width: expwidth - 30, height: expwidth - 30)
+                
+            }
+            
+            
+            if hueofexlosion > 0 {
+            hueofexlosion -= 1
+            }
+            
+            if expwidth > 250 {
+                fadealpha -= 1
+            }
+            
+           
+            
+            expwidth += 1
+            
+            }
         
         
-        canvas.fillColor = Color.green
-        canvas.drawEllipse(centreX: x, centreY: 50, width: 20, height: 20)
-        canvas.drawEllipse(centreX: xreverse, centreY: 150, width: 20, height: 20)
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 20, height: 20)
-        canvas.drawEllipse(centreX: xreverse, centreY: 350, width: 20, height: 20)
-        canvas.drawEllipse(centreX: x, centreY: 450, width: 20, height: 20)
         
-        
-        canvas.drawEllipse(centreX: 50, centreY: x, width: 20, height: 20)
-        canvas.drawEllipse(centreX: 150, centreY: xreverse, width: 20, height: 20)
-        canvas.drawEllipse(centreX: 250, centreY: x, width: 20, height: 20)
-        canvas.drawEllipse(centreX: 350, centreY: xreverse, width: 20, height: 20)
-        canvas.drawEllipse(centreX: 450, centreY: x, width: 20, height: 20)
-        
-        
-    }
+        }
     
 }
 
